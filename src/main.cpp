d706d38565764e53c6de49c1be812e2d9e379a55
YngVarr473 <igorkazakov682@gmail.com>
2025-03-18 21:57:52 +0500

    intial

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,10 @@
 #include <SDL3/SDL_main.h>
 #include <iostream>
 
-bool turnRect = false;
-bool turnRectY = true;
-float speed = 1;
-
 static SDL_Window *window;
 static SDL_Renderer *render;
 SDL_FRect rect = {12, 12, 72, 72};
+float speed = 3.2;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
@@ -21,16 +18,21 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     SDL_SetRenderDrawColor(render, 30, 30, 30, 185);
     SDL_RenderClear(render);
 
-    if (rect.y >= 1208 || rect.y <= 0){
-        turnRectY = !turnRectY;
-    }
-    rect.y = turnRect ? rect.y + speed : rect.y - speed;
+    const bool* keys = SDL_GetKeyboardState(NULL);
 
-    if (rect.x >= 648 || rect.x <= 0) {
-        turnRect = !turnRect;
+    if(keys[SDL_SCANCODE_W]){
+        rect.y -= speed;
     }
-    rect.x = turnRect ? rect.x + speed : rect.x - speed;
-    speed += 0.17;
+    if(keys[SDL_SCANCODE_S]){
+        rect.y += speed;
+    }
+    if(keys[SDL_SCANCODE_A]){
+        rect.x -= speed;
+    }
+    if(keys[SDL_SCANCODE_D]){
+        rect.x += speed;
+    }
+    
     SDL_SetRenderDrawColor(render, 120, 82, 148, 255);
     SDL_RenderFillRect(render, &rect);
     SDL_RenderPresent(render);
